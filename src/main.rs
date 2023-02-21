0c5ac6b068ec41db190a706e00ab173303669eaf
Satvik Kishore <satvik.kishore@gmail.com>
2023-02-21 20:53:15 +0000

    pnc

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,36 @@
 use text_io::read;
 
-fn fibo(n: u32) -> u32 {
-    if (n == 0) | (n == 1) {
-        return 1;
-    }
-    let mut x: u32 = 1;
-    let mut y: u32 = 1;
-    let mut ii: u32 = 0;
-    loop {
-        if ii==n {
-            break
-        }
-        let temp: u32 = x;
-        x = y;
-        y += temp;
-        ii += 1;
-    }
-    y
-}
-fn run_samples() {
-    println!("{}", fibo(0));
-    println!("{}", fibo(1));
-    println!("{}", fibo(2));
-    println!("{}", fibo(5));
-    println!("{}", fibo(10));
+struct  FailU32 {
+    number: u32,
+    fail: bool,
 }
 
+fn pnc(x: u32, y: u32) -> FailU32 {
+    if y > x {
+        return FailU32{number:0, fail:true};
+    }
+    let mut numerator: u32 = 1;
+    let mut denominator: u32 = 1;
+    let mut ii: u32 = 0;
+    loop {
+        if ii == (x-y){
+            break;
+        }
+        ii += 1;
+        numerator *= (x-y+ii-1);
+        denominator *= ii;
+    }
+    FailU32{number:numerator/denominator, fail:false}
+}
 fn main() {
-    run_samples();
-    let n: u32 = read!();
-    println!("your input: {}, output: {}", n, fibo(n));
+    println!("What is the number of total elements?");
+    let x: u32 = read!();
+    println!("What is the number of elements to choose?");
+    let y: u32 = read!();
+    let answer: FailU32 = pnc(x,y);
+    if answer.fail {
+        println!("Improper input")
+    } else {
+        println!("The Answer is {}", answer.number);
+    }
 }
