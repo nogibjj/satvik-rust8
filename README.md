99577cb3e087673a355b36cbe34093de3f2907c8
Satvik Kishore <satvik.kishore@gmail.com>
2023-04-06 02:37:57 +0000

    done

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-In this repository, we build a function to calculate mean and sd of five numbers from io
+In this repository, we build a function to reverse a string
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,13 @@
 use std::io::{self, BufRead};
 
 fn main() {
-    // Read a list of numbers from stdin
+    // Read a string from stdin
     let stdin = io::stdin();
-    let mut numbers = Vec::new();
-    let mut ii = 0;
-    for line in stdin.lock().lines() {
-        if let Ok(num) = line.unwrap().trim().parse::<f64>() {
-            numbers.push(num);
-        }
-        ii += 1;
-        if ii > 5{
-            break;
-        }
-    }
+    let input_string = stdin.lock().lines().next().unwrap().unwrap();
 
-    // Calculate the mean of the numbers
-    let mean = numbers.iter().sum::<f64>() / numbers.len() as f64;
+    // Reverse the string
+    let reversed_string = input_string.chars().rev().collect::<String>();
 
-    // Calculate the standard deviation of the numbers
-    let variance = numbers.iter().map(|x| (x - mean).powi(2)).sum::<f64>() / (numbers.len() - 1) as f64;
-    let std_dev = variance.sqrt();
-
-    // Print the results
-    println!("Mean: {}", mean);
-    println!("Standard deviation: {}", std_dev);
+    // Print the reversed string
+    println!("{}", reversed_string);
 }
