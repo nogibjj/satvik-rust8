d37c736e97cfb406f790c6111f06a8c4b165d491
Satvik Kishore <satvik.kishore@gmail.com>
2023-04-06 02:07:53 +0000

    done

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-In this repository, we build a function to list contents of a directory
+In this repository, we build a function to calculate mean and sd of five numbers from io
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
-use std::fs;
+use std::io::{self, BufRead};
 
 fn main() {
-    let dir_path = ".";//path to directory
-
-    let dir_contents = match fs::read_dir(dir_path) {
-        Ok(contents) => contents,
-        Err(e) => {
-            eprintln!("Error reading directory: {}", e);
-            return;
+    // Read a list of numbers from stdin
+    let stdin = io::stdin();
+    let mut numbers = Vec::new();
+    let mut ii = 0;
+    for line in stdin.lock().lines() {
+        if let Ok(num) = line.unwrap().trim().parse::<f64>() {
+            numbers.push(num);
         }
-    };
-
-    for entry in dir_contents {
-        match entry {
-            Ok(entry) => println!("{}", entry.path().display()),
-            Err(e) => eprintln!("Error reading directory entry: {}", e),
+        ii += 1;
+        if ii > 5{
+            break;
         }
     }
+
+    // Calculate the mean of the numbers
+    let mean = numbers.iter().sum::<f64>() / numbers.len() as f64;
+
+    // Calculate the standard deviation of the numbers
+    let variance = numbers.iter().map(|x| (x - mean).powi(2)).sum::<f64>() / (numbers.len() - 1) as f64;
+    let std_dev = variance.sqrt();
+
+    // Print the results
+    println!("Mean: {}", mean);
+    println!("Standard deviation: {}", std_dev);
 }
