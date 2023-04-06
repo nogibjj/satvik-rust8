36c4947d61ea8097bb93225df379b264d3ffd891
Satvik Kishore <satvik.kishore@gmail.com>
2023-04-06 01:51:44 +0000

    done

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-In this repository, we build a linked list
+In this repository, we build a function to list contents of a directory
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,20 @@
-struct Node<T> {
-    value: T,
-    next: Option<Box<Node<T>>>,
-}
-
-impl<T> Node<T> {
-    fn new(value: T) -> Node<T> {
-        Node { value, next: None }
-    }
-}
-
-pub struct LinkedList<T> {
-    head: Option<Box<Node<T>>>,
-}
-
-impl<T> Default for LinkedList<T> {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl<T> LinkedList<T> {
-    pub fn new() -> LinkedList<T> {
-        LinkedList { head: None }
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.head.is_none()
-    }
-    pub fn push_front(&mut self, value: T) {
-        let mut new_node = Node::new(value);
-        new_node.next = self.head.take();
-        self.head = Some(Box::new(new_node));
-    }
-
-}
+use std::fs;
 
 fn main() {
-    let n0 = Node { value: 0, next: None };
-    let mut ll = LinkedList{head: Some(Box::new(n0))};
-    ll.push_front(42)
+    let dir_path = ".";//path to directory
+
+    let dir_contents = match fs::read_dir(dir_path) {
+        Ok(contents) => contents,
+        Err(e) => {
+            eprintln!("Error reading directory: {}", e);
+            return;
+        }
+    };
+
+    for entry in dir_contents {
+        match entry {
+            Ok(entry) => println!("{}", entry.path().display()),
+            Err(e) => eprintln!("Error reading directory entry: {}", e),
+        }
+    }
 }
